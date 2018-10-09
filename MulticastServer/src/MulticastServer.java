44fa616161addacfd28dfe78bf01eae6407c9756
dimmaski <diogo.fernandes.12@hotmail.com>
2018-10-09 20:39:23 +0100

    Multicast server a receber packets + thread de resposta

--- MulticastServer/src/MulticastServer.java
+++ MulticastServer/src/MulticastServer.java
@@ -5,33 +5,33 @@ import java.io.IOException;
 
 public class MulticastServer extends Thread {
     private String MULTICAST_ADDRESS = "224.3.2.1";
-    private int PORT = 4321;
-    private long SLEEP_TIME = 5000;
+    private int PORT = 5214;
+    private MulticastSocket socket = null;
 
     public static void main(String[] args) {
         MulticastServer server = new MulticastServer();
         server.start();
     }
 
-    public MulticastServer() {
-        super("Server " + (long) (Math.random() * 1000));
-    }
 
     public void run() {
-        MulticastSocket socket = null;
-        long counter = 0;
-        System.out.println(this.getName() + " running...");
+
+        // wait for packets
+
         try {
-            socket = new MulticastSocket();  // create socket without binding it (only for sending)
+            socket = new MulticastSocket(PORT);  // create socket and bind it
+            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+            socket.joinGroup(group);
+
+
             while (true) {
-                String message = this.getName() + " packet " + counter++;
-                byte[] buffer = message.getBytes();
+                byte[] buffer = new byte[256];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
 
-                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
-                socket.send(packet);
-
-                try { sleep((long) (Math.random() * SLEEP_TIME)); } catch (InterruptedException e) { }
+                MulticastServerResponse threadToResolvePacket = new MulticastServerResponse(packet, PORT, MULTICAST_ADDRESS);
+                threadToResolvePacket.start();
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,4 +39,5 @@ public class MulticastServer extends Thread {
             socket.close();
         }
     }
+
 }
