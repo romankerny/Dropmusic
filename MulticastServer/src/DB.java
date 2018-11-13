cc4cfa3c806ba447075cda54d9b8df099325f6c4
dimmaski <diogo.fernandes.12@hotmail.com>
2018-11-13 01:31:33 +0000

    addUser DB

--- MulticastServer/src/DB.java
+++ MulticastServer/src/DB.java
@@ -32,4 +32,35 @@ public class DB {
         return rspToMulticast;
     }
 
+    public static String insertArtist(String name, String details, Connection con) {
+
+        PreparedStatement pstmt = null;
+        String rspToMulticast = null;
+        int rs;
+
+        try {
+            pstmt = con.prepareStatement("INSERT INTO artist (name, details) VALUES (?,?)");
+            pstmt.setString(1, name);
+            pstmt.setString(2, details);
+            rs = pstmt.executeUpdate();
+
+            rspToMulticast = "Artist " + name + " added to DB with sucsses";
+            System.out.println("Inserted " + rs + " new artist(s).");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            switch (e.getErrorCode()) {
+                case 1062:
+                    // duplicate entry
+                    System.out.println("Got ERROR:1062");
+                    rspToMulticast = "Artist : " + name +" already exists.";
+                    break;
+            }
+
+        }
+        return rspToMulticast;
+
+    }
+
 }
