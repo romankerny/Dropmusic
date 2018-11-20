017ed74496e7b3cd8760fe228a995382788407b5
Roman Kernychyshyn <romanvk@student.dei.uc.pt>
2018-11-20 21:54:15 +0000

    DELETE CASCADE

--- create_tables.sql
+++ create_tables.sql
@@ -99,36 +99,36 @@ CREATE TABLE shows_artist (
 	PRIMARY KEY(shows_id,artist_name)
 );
 
-ALTER TABLE album ADD CONSTRAINT album_fk1 FOREIGN KEY (artist_name) REFERENCES artist(name);
+ALTER TABLE album ADD CONSTRAINT album_fk1 FOREIGN KEY (artist_name) REFERENCES artist(name) ON DELETE CASCADE;
 ALTER TABLE album ADD CONSTRAINT main_constrains CHECK (id != "" AND title != "" AND description != "" AND genre != "" AND editor_label != "");
 ALTER TABLE album ADD CONSTRAINT datas CHECK (launch_date > date('1900-01-01') AND launch_date < sysdate);
-ALTER TABLE review ADD CONSTRAINT review_fk1 FOREIGN KEY (user_email) REFERENCES user(email);
-ALTER TABLE review ADD CONSTRAINT review_fk2 FOREIGN KEY (album_id) REFERENCES album(id);
+ALTER TABLE review ADD CONSTRAINT review_fk1 FOREIGN KEY (user_email) REFERENCES user(email) ON DELETE CASCADE;
+ALTER TABLE review ADD CONSTRAINT review_fk2 FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE;
 ALTER TABLE review ADD CONSTRAINT critic CHECK (critic != "" );
 ALTER TABLE review ADD CONSTRAINT rating CHECK (rating >= 0 AND rating <= 5);
 ALTER TABLE user ADD CONSTRAINT email CHECK (email like '%@%.%' AND email != "");
 ALTER TABLE user ADD CONSTRAINT password CHECK (password != "" );
-ALTER TABLE music ADD CONSTRAINT music_fk1 FOREIGN KEY (album_id) REFERENCES album(id);
+ALTER TABLE music ADD CONSTRAINT music_fk1 FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE;
 ALTER TABLE music ADD CONSTRAINT id CHECK (id != "");
 ALTER TABLE music ADD CONSTRAINT track CHECK (track > 0 );
 ALTER TABLE music ADD CONSTRAINT title CHECK (title != "");
-ALTER TABLE notification ADD CONSTRAINT notification_fk1 FOREIGN KEY (user_email) REFERENCES user(email);
+ALTER TABLE notification ADD CONSTRAINT notification_fk1 FOREIGN KEY (user_email) REFERENCES user(email) ON DELETE CASCADE;
 ALTER TABLE notification ADD CONSTRAINT notification CHECK (text != "");
-ALTER TABLE upload ADD CONSTRAINT upload_fk1 FOREIGN KEY (music_id) REFERENCES music(id);
-ALTER TABLE upload ADD CONSTRAINT upload_fk2 FOREIGN KEY (user_email) REFERENCES user(email);
+ALTER TABLE upload ADD CONSTRAINT upload_fk1 FOREIGN KEY (music_id) REFERENCES music(id) ON DELETE CASCADE;
+ALTER TABLE upload ADD CONSTRAINT upload_fk2 FOREIGN KEY (user_email) REFERENCES user(email) ON DELETE CASCADE;
 ALTER TABLE upload ADD CONSTRAINT constraint_0 CHECK (musicFileName != "");
-ALTER TABLE playlist ADD CONSTRAINT playlist_fk1 FOREIGN KEY (user_email) REFERENCES user(email);
+ALTER TABLE playlist ADD CONSTRAINT playlist_fk1 FOREIGN KEY (user_email) REFERENCES user(email) ON DELETE CASCADE;
 ALTER TABLE playlist ADD CONSTRAINT name CHECK (name != "");
-ALTER TABLE upload_user ADD CONSTRAINT upload_user_fk1 FOREIGN KEY (upload_music_id) REFERENCES upload(music_id);
-ALTER TABLE upload_user ADD CONSTRAINT upload_user_fk2 FOREIGN KEY (upload_user_email) REFERENCES upload(user_email);
-ALTER TABLE upload_user ADD CONSTRAINT upload_user_fk3 FOREIGN KEY (user_email) REFERENCES user(email);
-ALTER TABLE artist_user ADD CONSTRAINT artist_user_fk1 FOREIGN KEY (artist_name) REFERENCES artist(name);
-ALTER TABLE artist_user ADD CONSTRAINT artist_user_fk2 FOREIGN KEY (user_email) REFERENCES user(email);
-ALTER TABLE music_playlist ADD CONSTRAINT music_playlist_fk1 FOREIGN KEY (music_id) REFERENCES music(id);
-ALTER TABLE music_playlist ADD CONSTRAINT music_playlist_fk2 FOREIGN KEY (playlist_name) REFERENCES playlist(name);
-ALTER TABLE music_playlist ADD CONSTRAINT music_playlist_fk3 FOREIGN KEY (playlist_user_email) REFERENCES playlist(user_email);
-ALTER TABLE shows_artist ADD CONSTRAINT shows_artist_fk1 FOREIGN KEY (shows_id) REFERENCES shows(id);
-ALTER TABLE shows_artist ADD CONSTRAINT shows_artist_fk2 FOREIGN KEY (artist_name) REFERENCES artist(name);
+ALTER TABLE upload_user ADD CONSTRAINT upload_user_fk1 FOREIGN KEY (upload_music_id) REFERENCES upload(music_id) ON DELETE CASCADE;
+ALTER TABLE upload_user ADD CONSTRAINT upload_user_fk2 FOREIGN KEY (upload_user_email) REFERENCES upload(user_email) ON DELETE CASCADE;
+ALTER TABLE upload_user ADD CONSTRAINT upload_user_fk3 FOREIGN KEY (user_email) REFERENCES user(email) ON DELETE CASCADE;
+ALTER TABLE artist_user ADD CONSTRAINT artist_user_fk1 FOREIGN KEY (artist_name) REFERENCES artist(name) ON DELETE CASCADE;
+ALTER TABLE artist_user ADD CONSTRAINT artist_user_fk2 FOREIGN KEY (user_email) REFERENCES user(email) ON DELETE CASCADE;
+ALTER TABLE music_playlist ADD CONSTRAINT music_playlist_fk1 FOREIGN KEY (music_id) REFERENCES music(id) ON DELETE CASCADE;
+ALTER TABLE music_playlist ADD CONSTRAINT music_playlist_fk2 FOREIGN KEY (playlist_name) REFERENCES playlist(name) ON DELETE CASCADE;
+ALTER TABLE music_playlist ADD CONSTRAINT music_playlist_fk3 FOREIGN KEY (playlist_user_email) REFERENCES playlist(user_email) ON DELETE CASCADE;
+ALTER TABLE shows_artist ADD CONSTRAINT shows_artist_fk1 FOREIGN KEY (shows_id) REFERENCES shows(id) ON DELETE CASCADE;
+ALTER TABLE shows_artist ADD CONSTRAINT shows_artist_fk2 FOREIGN KEY (artist_name) REFERENCES artist(name) ON DELETE CASCADE;
 
 
 INSERT into user (email, password, editor) VALUES ('admin', 'admin', true);
